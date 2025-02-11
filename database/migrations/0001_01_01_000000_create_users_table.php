1e0c38fe8ad743c4e9e9404ff75bac05e179f16a
devFarjanur <farjanur15-2956@diu.edu.bd>
2025-02-11 23:50:59 +0600

    Bidding Project

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('phone');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('profile_image')->nullable();
+            $table->enum('role', ['customer', 'vendor', 'admin'])->default('customer');
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
