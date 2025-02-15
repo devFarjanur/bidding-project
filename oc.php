9a0a98d8d6bd6bcdf26c0c64b1dc217c43ef714c
devFarjanur <farjanur15-2956@diu.edu.bd>
2025-02-16 00:21:18 +0600

    Test

--- oc.php
+++ oc.php
@@ -9,10 +9,10 @@ $output = shell_exec('php artisan optimize:clear 2>&1');
 // Check if the command ran successfully by checking the output for specific success messages
 if (strpos($output, 'Cleared!') !== false) {
     // Display the output
-    echo "<pre>$output</pre>";
+    // echo "<pre>$output</pre>";
     echo "<h2>✅ Command executed successfully!</h2>";
 } else {
     // Display the output if there is an error
-    echo "<pre>$output</pre>";
+    // echo "<pre>$output</pre>";
     echo "<h2>❌ There was an error executing the command.</h2>";
 }
